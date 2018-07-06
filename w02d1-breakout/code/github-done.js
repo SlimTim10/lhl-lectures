849ed9bc4ac29c1fc32e7db47e487282105debd7
SlimTim10 <slimtim10@gmail.com>
2018-07-06 17:36:27 -0400

    Use GitHub API key

--- w02d1-breakout/code/github-done.js
+++ w02d1-breakout/code/github-done.js
@@ -1,6 +1,7 @@
 // A node script that, given a repo, prints out the latest 20 issues for the repo and format the issue differently if they are closed vs open
 
 var request = require('request');
+require('dotenv').config();
 
 var base = 'https://api.github.com';
 var repo = '/repos/facebook/react/issues?state=all';
@@ -8,7 +9,8 @@ var repo = '/repos/facebook/react/issues?state=all';
 var options = {
   url: base+repo,
   headers: {
-    'User-Agent': 'myscript'
+    'User-Agent': 'myscript',
+    'Authorization': 'token ' + process.env.GITHUB_API_KEY
   }
 };
  
@@ -28,15 +30,18 @@ function callback(error, response, body) {
     console.log('---OPEN---');
     for (var issue of openIssues) {
       console.log(issue.body);
-      console.log('---------------------------------');
+      console.log('------------------------------------------------------------------------------------------');
     }
 
     console.log('---CLOSED---');
     for (var issue of closedIssues) {
       console.log(issue.body);
-      console.log('---------------------------------');
+      console.log('------------------------------------------------------------------------------------------');
     }
-
+  } else {
+    console.log('Status code:', response.statusCode);
+    console.log('Error:', error);
+    console.log('Body:', body);
   }
 }
  

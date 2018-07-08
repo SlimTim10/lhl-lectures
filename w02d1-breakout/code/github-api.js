8ad77dad08add3f69bd982a85907c2289d2e8dfd
SlimTim10 <slimtim10@gmail.com>
2018-07-08 11:13:29 -0400

    Break into separate module

---
+++ w02d1-breakout/code/github-api.js
@@ -0,0 +1,41 @@
+var request = require('request');
+
+function handleResponse(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var issues = JSON.parse(body).slice(0, 20);
+    var closedIssues = issues.filter(function(issue) {
+      return issue.state === 'closed';
+    });
+    var openIssues = issues.filter(function(issue) {
+      return issue.state === 'open';
+    });
+    console.log('CLOSED ISSUES:');
+    for (var issue of closedIssues) {
+      console.log('Title: ' + issue.title);
+    }
+    console.log('OPEN ISSUES:');
+    for (var issue of openIssues) {
+      console.log('Title: ' + issue.title);
+    }
+  } else {
+    console.log('ERROR:', body);
+  }
+}
+
+function logIssues(owner, repo) {
+  var API_BASE = 'https://api.github.com';
+  var ISSUES_ENDPOINT = API_BASE + '/repos/' + owner + '/' + repo + '/issues?state=all';
+  
+  var options = {
+    url: ISSUES_ENDPOINT,
+    headers: {
+      'User-Agent': 'myawesomeapp'
+    }
+  };
+  
+  request(options, handleResponse);
+}
+
+module.exports = {
+  logIssues: logIssues
+};
--- w02d1-breakout/code/github-issues.js
+++ w02d1-breakout/code/github-issues.js
@@ -1,39 +1,7 @@
-var request = require('request');
+var githubApi = require('./github-api');
 
 var args = process.argv.slice(2);
 var owner = args[0];
 var repo = args[1];
 
-var API_BASE = 'https://api.github.com';
-var ISSUES_ENDPOINT = API_BASE + '/repos/' + owner + '/' + repo + '/issues?state=all';
- 
-var options = {
-  url: ISSUES_ENDPOINT,
-  headers: {
-    'User-Agent': 'myawesomeapp'
-  }
-};
-
-function callback(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var issues = JSON.parse(body).slice(0, 20);
-    var closedIssues = issues.filter(function(issue) {
-      return issue.state === 'closed';
-    });
-    var openIssues = issues.filter(function(issue) {
-      return issue.state === 'open';
-    });
-    console.log('CLOSED ISSUES:');
-    for (var issue of closedIssues) {
-      console.log('Title: ' + issue.title);
-    }
-    console.log('OPEN ISSUES:');
-    for (var issue of openIssues) {
-      console.log('Title: ' + issue.title);
-    }
-  } else {
-    console.log('ERROR:', body);
-  }
-}
-
-request(options, callback);
+githubApi.logIssues(owner, repo);

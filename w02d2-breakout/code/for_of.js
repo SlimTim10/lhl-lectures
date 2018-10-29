c89e25c8601e8cd768c848f2606b4a3b74f58ac5
SlimTim10 <slimtim10@gmail.com>
2018-10-28 23:01:39 -0400

    Add notes for for...of

---
+++ w02d2-breakout/code/for_of.js
@@ -0,0 +1,54 @@
+function old_way() {
+  var names = ['Alice', 'Bob', 'Carol', 'Dean'];
+
+  for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+  }
+}
+
+// old_way();
+
+function new_way() {
+  const names = ['Alice', 'Bob', 'Carol', 'Dean'];
+
+  for (let name of names) {
+    console.log(name);
+  }
+}
+
+// new_way();
+
+function other() {
+  const names = ['Alice', 'Bob', 'Carol', 'Dean'];
+  
+  for (let name in names) {
+    console.log(name);
+  }
+}
+
+// other(); // Will this work?
+
+function another() {
+  const people = {
+    Alice: 25,
+    Bob: 26,
+    Carol: 27,
+    Dean: 28
+  };
+
+  for (let person of people) {
+    console.log(age);
+  }
+}
+
+// another(); // Will this work?
+
+function how_about_strings() {
+  const sentence = 'ES6 is better than ES2015';
+
+  for (let letter of sentence) {
+    console.log(letter);
+  }
+}
+
+how_about_strings(); // Will this work?

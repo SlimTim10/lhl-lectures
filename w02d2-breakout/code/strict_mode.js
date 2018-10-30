d5270fb0c43164755d2ac023f687c2ce074d2365
SlimTim10 <slimtim10@gmail.com>
2018-10-29 20:13:32 -0400

    Add examples for strict mode

---
+++ w02d2-breakout/code/strict_mode.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var highScore = 666;
+
+// highScore = 1024;
+
+// highscore = 1024;
+
+var goodFood = ['apple', 'banana'];
+
+// goodFood.push('carrot');
+
+// goodfood.push('carrot');
